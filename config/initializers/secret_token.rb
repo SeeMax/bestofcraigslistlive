9d1d0c55b8d3988ecdc32fcd25437052c747425a
Max Mearsheimer <mmearshe@gmail.com>
2014-04-22 14:41:12 -0500

    blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bestofcraigslist::Application.config.secret_key_base = '3fe133a64664700e237649ca7c0fe6e2085bfe88859a6c17a0329a9354eb4091fb6de3eb986563551d1a39e3f5860ea7c70825f12e2da32450eedd26943e3004'
